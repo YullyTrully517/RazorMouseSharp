beb0f68d8c84d616f14dd4aa22bd8523ea4ecd0f
YullyTrully517 <75863762+YullyTrully517@users.noreply.github.com>
2022-09-04 20:33:39 -0500

    Add files via upload

---
+++ RazorMouse/Mouse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorMouse
+{
+    class Razor
+    {
+        enum DriverErrorCallback
+        {
+            Error = 1,
+            Success = 2
+        }
+        enum MouseErrorCallback
+        {
+            Success = 1,
+        }
+        [DllImport("razor.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int InitMouse();
+
+        [DllImport("razor.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int MoveMouse(int x, int y, bool from_start_point);
+        public static void LoadMouseDriver()
+        {
+            Console.WriteLine($"    Callback -> {InitMouse()}");
+
+            switch(InitMouse())
+            {
+                case (int)DriverErrorCallback.Success:
+                    Console.WriteLine("    Sucessfully Loaded Driver!");
+                    break;
+                case (int)DriverErrorCallback.Error:
+                    Console.WriteLine("    Failed To Load Mouse Driver!");
+                    break;
+            }
+        }
+
+        public static void MouseMove(int x, int y)
+        {
+            if (MoveMouse(x, y, false) == (int)MouseErrorCallback.Success)
+            {
+                Console.WriteLine("    Moved Mouse Sucessfully!");
+            }
+            else { Console.WriteLine("    Failed To Move Mouse"); }
+        }
+    }
+}
---
+++ RazorMouse/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorMouse
+{
+    class Program
+    {
+        //Credits To For Main Project https://github.com/Sadmeme/rzctl
+        //Credits For wrapper yully1337 on uc
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Title = "Mouse Wrapper : Yully";
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Razor.LoadMouseDriver();
+
+                Console.ReadLine();
+
+                Console.Clear();
+
+                MouseMoveEvent:
+
+                Console.Clear();
+
+                Console.WriteLine();
+
+                string X;
+                string Y;
+
+                Console.Clear();
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.Write("    Input X: "); X = Console.ReadLine();
+
+                Console.Clear();
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.Write("    Input Y: "); Y = Console.ReadLine();
+
+                Razor.MouseMove(Convert.ToInt32(X), Convert.ToInt32(Y));
+
+                goto MouseMoveEvent;
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine($"    Exception Thrown : {ex.Message}");
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
